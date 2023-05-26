90033ba24465a4701f99ac5f88528b804b9ab377
wealthybeans <wealthybeans@wealthybeanss-MacBook-Air.local>
2023-05-25 17:39:59 -0700

    define imports

--- src/lib/decorators.ts
+++ src/lib/decorators.ts
@@ -17,3 +17,5 @@ export function LazyLoad(moduleFn: () => Promise<NgModule>) {
 
 
 
+
+
---
+++ src/lib/routing.service.ts
@@ -0,0 +1,8 @@
+
+// ROUTE PARAMETERS AND DATA MANAGEMENT
+
+
+// provides methods for accessing and manipulating route parameters and sharing data between routes:
+
+import { Injectable } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
