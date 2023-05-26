df7fa1c6e159bf2f0af751fc7ea2376c5741b9a0
wealthybeans <wealthybeans@wealthybeanss-MacBook-Air.local>
2023-05-25 17:35:41 -0700

    import ngmodule

--- src/lib/decorators.ts
+++ src/lib/decorators.ts
@@ -1,8 +1,11 @@
 import { Route } from "@angular/router";
+import { NgModule } from "@angular/core";
 
-
+//route config
 export function RouteConfig(route: Route) {
     return function(target: any) {
         Reflect.defineMetadata('routeConfig', route, target);
     }
 }
+
+
