0fcfb5bb16edee75decebe3ffb4855d201caf602
wealthybeans <wealthybeans@wealthybeanss-MacBook-Air.local>
2023-05-25 17:47:41 -0700

    implement methods

--- src/lib/routing.service.ts
+++ src/lib/routing.service.ts
@@ -5,14 +5,19 @@
 // provides methods for accessing and manipulating route parameters and sharing data between routes:
 
 import { Injectable } from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, ActivatedRoute, Params, Router } from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, ActivatedRoute, Params, Router, CanActivateFn, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 
-export class RoutingService {
+export class RoutingService implements CanActivate {
     constructor(private router: Router, private route: ActivatedRoute) {}
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+        // Implement your guard logic here
+        return true; // Or perform actual guard checks
+      }
     
     getRouteParams(): Params {
         return this.route.snapshot.params;
@@ -25,5 +30,16 @@ export class RoutingService {
     //now add route guards
 
 
+
+    // add HELPERS FOR ROUTING TASKS
+
+    getCurrentRoute(): string {
+        return this.router.url;
+    }
+
+    getQueryParams(): Params {
+        return this.route.snapshot.queryParams;
+        
+    }
 
 }
